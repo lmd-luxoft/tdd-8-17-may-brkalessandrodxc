dc78aed6958cf142b5312e8ba881309a2edf3dbf
Alessandro Branchetti <a.branchetti@dxc.com>
2023-05-10 10:33:58 +0200

    green tests for valid input

--- TDDKataCalc/Calc.cs
+++ TDDKataCalc/Calc.cs
@@ -8,7 +8,7 @@
                 return 0;
 
             if (digits.Length == 1)
-                return -1;
+                return Int16.Parse(digits);
 
             var numbers = digits.Split(',');
             return Int16.Parse(numbers[0]) + Int16.Parse((numbers[1]));
